667d34f4b5176982c86b3c4853751b3e3cf8d2da
keitaiida <kiida@protein.osaka-u.ac.jp>
2022-01-30 13:16:29 +0900

    Commit data/.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------80
+#
+#-----------------------------------------------------------------------------80
+#' Single-cell RNA-seq data of peripheral blood mononuclear cells (PBMC 4k)
+#'
+#' A scRNA-seq dataset processed by conservative quality control.
+#' The original data "4k PBMCs from a Healthy Donor" was downloaded from 10x
+#' Genomics database.
+#'
+#' @format Read count table
+#' \describe{
+#'   Gene-by-cell matrix data.
+#' }
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression}
+"pbmc_counts"
