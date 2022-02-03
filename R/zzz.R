0ebfb8068211bba72875a0daf84bc5b44a534cff
Johannes Nicolaus Wibisana <johannes.nicolaus@gmail.com>
2022-02-03 11:33:28 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onAttach <- function(){ 
-  msg <- paste0("Thank you for using ", "ASURAT", "\n\n", 
-                "Please cite ", "ASURAT", " using the following:\n", 
+  msg <- paste0("Please cite ", "ASURAT", " using the following:\n", 
                 "Iida, K., Kondo, J., Wibisana, J.N., Inoue M., Okada M., 2021. BiorXiv.
   ASURAT: functional annotation-driven unsupervised clustering of single-cell transcriptomes.\nhttps://doi.org/10.1101/2021.06.09.447731
   ")
