97fd29b20e40ebe1abf2d762c5f7489eb051f94c
keitaiida <kiida@protein.osaka-u.ac.jp>
2022-02-04 17:14:09 +0900

    Commit R/find_marker_signs.R.

--- R/find_marker_signs.R
+++ R/find_marker_signs.R
@@ -41,7 +41,10 @@ compute_nswaps <- function(vec1 = NULL, vec2 = NULL){
   compute_nswap_001 <- "int compute_nswap_001(NumericVector enco){
     int  swaps = 0;
     for(int i=0; i<(enco.length()-1); ++i){
-      for(int j=i+1; j<enco.length(); ++j){
+      for(int j=0; j<enco.length(); ++j){
+        if(j <= i){
+          continue;
+        }
         if(enco[i] > enco[j]){
           swaps++;
         }
